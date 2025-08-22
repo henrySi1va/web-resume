71fe9381a90207a06a7028571b64d9af0ba3b72f
HenrySi1va <henry@henrysilva.xyz>
2025-08-22 02:03:26 +0000

    Comment out search bar for now

--- web-resume/src/components/Header.vue
+++ web-resume/src/components/Header.vue
@@ -5,12 +5,13 @@
     <!-- Header -->
     <header class="mb-8 text-center">
       <h1 class="text-3xl font-bold">Henry Silva Technical Resume</h1>
-      <div class="mt-4 max-w-lg mx-auto">
+      <!-- Comment out search bar for now -->
+      <!-- <div class="mt-4 max-w-lg mx-auto">
         <input
           type="text"
           placeholder="Search projects..."
           class="w-full px-4 py-2 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-teal-500"
         />
-      </div>
+      </div> -->
     </header>
 </template>
