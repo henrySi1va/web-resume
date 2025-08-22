6a44f2b5b5b7c13d5ce27249da2467456ea73338
HenrySi1va <henry@henrysilva.xyz>
2025-08-22 02:41:37 +0000

    Finish first draft

--- web-resume/src/components/Project.vue
+++ web-resume/src/components/Project.vue
@@ -9,28 +9,30 @@
 </script>
 
 <template>
-  <h2 class="text-xl font-semibold mb-2">{{ project.title }}</h2>
-  <p class="mb-4 text-gray-700">{{ project.description }}</p>
+  <div class="pb-10 border-b border-gray-200">
+    <h2 class="text-xl font-semibold mb-2">{{ project.title }}</h2>
+    <p class="mb-4 text-gray-700">{{ project.description }}</p>
 
-  <ul class="space-y-1 text-sm">
-    <li><span class="font-bold">Year:</span> <span>{{ project.year }}</span></li>
-    <li><span class="font-bold">Role:</span> <span>{{ project.role }}</span></li>
-    <li><span class="font-bold">Industry:</span> <span>{{ project.industry }}</span></li>
-    <li><span class="font-bold">Tools:</span> <span>{{ project.tools }}</span></li>
-    <li><span class="font-bold">Tech:</span> <span>{{ project.tech }}</span></li>
-    <li>
-      <span class="font-bold">Links:</span>
-      <span>
-        <a
-          v-for="link in project.links"
-          :key="link.url"
-          :href="link.url"
-          class="text-teal-600 hover:underline mr-3"
-          target="_blank"
-        >
-          {{ link.label }}
-        </a>
-      </span>
-    </li>
-  </ul>
+    <ul class="space-y-1 text-sm">
+      <li><span class="font-bold">Year:</span> <span>{{ project.year }}</span></li>
+      <li><span class="font-bold">Role:</span> <span>{{ project.role }}</span></li>
+      <li><span class="font-bold">Industry:</span> <span>{{ project.industry }}</span></li>
+      <li><span class="font-bold">Tools:</span> <span>{{ project.tools }}</span></li>
+      <li><span class="font-bold">Tech:</span> <span>{{ project.tech }}</span></li>
+      <li>
+        <span class="font-bold">Links:</span>
+        <span>
+          <a
+            v-for="link in project.links"
+            :key="link.url"
+            :href="link.url"
+            class="text-teal-600 hover:underline mr-3"
+            target="_blank"
+          >
+            {{ link.label }}
+          </a>
+        </span>
+      </li>
+    </ul>
+  </div>
 </template>
--- web-resume/src/components/ProjectList.vue
+++ web-resume/src/components/ProjectList.vue
@@ -1,45 +1,41 @@
 <script setup>
+  import {ref, onMounted } from 'vue';
   import Project from '@/components/Project.vue';
+  
+  // Load projects from public/projects.json
+  const projects = ref([]);
+  const isLoading = ref(true);
+  const error = ref(null);
 
-  const projects = [
-    {
-      id: 1,
-      title: "IoT Sensor Platform",
-      description:
-        "Developed a full-stack IoT platform to collect, analyze, and visualize environmental data in real time.",
-      year: "2023",
-      role: "Lead Engineer",
-      industry: "Industrial IoT",
-      tools: "Docker, GitHub Actions, Grafana",
-      tech: "C++, Zephyr RTOS, Node.js",
-      links: [{ label: "GitHub", url: "#" }],
-    },
-    {
-      id: 2,
-      title: "Task Tracking Web App",
-      description:
-        "Built a lightweight task tracker to explore front-end development with Vue.js and MongoDB.",
-      year: "2024",
-      role: "Full Stack Developer",
-      industry: "Software Tools",
-      tools: "VS Code, MongoDB Atlas, Tailwind",
-      tech: "Vue.js, Express.js, MongoDB",
-      links: [
-        { label: "Demo", url: "#" },
-        { label: "GitHub", url: "#" },
-      ],
-    },
-  ];
+  onMounted(async () => {
+    try {
+      const res = await fetch("/projects.json"); // at public/projects.json
+
+      if (!res.ok) throw new Error('HTTP Error! Status: ${ res.status }');
+
+      projects.value = await res.json();
+    } catch (err) {
+      error.value = err.message;
+    } finally {
+      isLoading.value = false;
+    }
+  });
 </script>
 
 <template>
   <!-- Project List -->
   <section class="space-y-8">
-    <Project
-      v-for="project in projects"
-      :key="project.id"
-      :project="project"
-      class="pb-6 border-b border-gray-400"
-    />
+    <div v-if="isLoading">Loading Projects ...</div>
+    <div v-else-if="error">
+      Loading Projects Failed
+      <p> {{ error }} </p>
+    </div>
+    <div v-else>
+      <Project
+        v-for="project in projects"
+        :key="project.id"
+        :project="project"
+      />
+    </div>
   </section>
 </template>
